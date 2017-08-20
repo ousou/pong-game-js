a6e7d0dc591e7161f2b9999b33d25ebb5dcb2470
Sebastian Björkqvist <sebastian@sebastianbjorkqvist.com>
2017-08-20 11:46:41 +0300

    Small refactoring of state variable

--- js/game.js
+++ js/game.js
@@ -19,10 +19,11 @@ pong.constants.paddle2.leftKey = 90;
 pong.constants.paddle2.rightKey = 88;
 
 pong.state = {};
-pong.state.x = pong.canvas.width / 2;
-pong.state.y = pong.canvas.height - 30;
-pong.state.dx = 2;
-pong.state.dy = -2;
+pong.state.ball = {};
+pong.state.ball.x = pong.canvas.width / 2;
+pong.state.ball.y = pong.canvas.height - 30;
+pong.state.ball.dx = 2;
+pong.state.ball.dy = -2;
 
 pong.state.paddle1 = {};
 pong.state.paddle1.right = false;
@@ -40,10 +41,11 @@ pong.score = {
 }
 
 function resetState() {
-    pong.state.x = pong.canvas.width / 2;
-    pong.state.y = pong.canvas.height - 30;
-    pong.state.dx = 2;
-    pong.state.dy = -2;
+    pong.state.ball = {};
+    pong.state.ball.x = pong.canvas.width / 2;
+    pong.state.ball.y = pong.canvas.height - 30;
+    pong.state.ball.dx = 2;
+    pong.state.ball.dy = -2;
 
     pong.state.paddle1 = {};
     pong.state.paddle1.right = false;
@@ -54,7 +56,6 @@ function resetState() {
     pong.state.paddle2.x = (pong.canvas.width - pong.constants.paddleWidth) / 2;
     pong.state.paddle2.right = false;
     pong.state.paddle2.left = false;
-
 }
 
 function drawBall(ctx, xCoord, yCoord, radius, color) {
@@ -74,18 +75,18 @@ function drawPaddle(ctx, xCoord, yCoord, height, width, color) {
 }
 
 function checkCollisions() {
-    if (pong.state.x + pong.state.dx > pong.canvas.width - pong.constants.ballRadius || pong.state.x + pong.state.dx < pong.constants.ballRadius) {
-        pong.state.dx = -pong.state.dx;
+    if (pong.state.ball.x + pong.state.ball.dx > pong.canvas.width - pong.constants.ballRadius || pong.state.ball.x + pong.state.ball.dx < pong.constants.ballRadius) {
+        pong.state.ball.dx = -pong.state.ball.dx;
     }
-    if (pong.state.y + pong.state.dy < pong.constants.ballRadius) {
-        if (pong.state.x > pong.state.paddle2.x && pong.state.x < pong.state.paddle2.x + pong.constants.paddleWidth) {
-            pong.state.dy = -pong.state.dy;
+    if (pong.state.ball.y + pong.state.ball.dy < pong.constants.ballRadius) {
+        if (pong.state.ball.x > pong.state.paddle2.x && pong.state.ball.x < pong.state.paddle2.x + pong.constants.paddleWidth) {
+            pong.state.ball.dy = -pong.state.ball.dy;
         } else {
             handleGameOver("1");
         }
-    } else if (pong.state.y + pong.state.dy > pong.canvas.height - pong.constants.ballRadius) {
-        if (pong.state.x > pong.state.paddle1.x && pong.state.x < pong.state.paddle1.x + pong.constants.paddleWidth) {
-            pong.state.dy = -pong.state.dy;
+    } else if (pong.state.ball.y + pong.state.ball.dy > pong.canvas.height - pong.constants.ballRadius) {
+        if (pong.state.ball.x > pong.state.paddle1.x && pong.state.ball.x < pong.state.paddle1.x + pong.constants.paddleWidth) {
+            pong.state.ball.dy = -pong.state.ball.dy;
         } else {
             handleGameOver("2");
         }
@@ -99,8 +100,8 @@ function handleGameOver(winner) {
 }
 
 function updateBallLocation() {
-    pong.state.x += pong.state.dx;
-    pong.state.y += pong.state.dy;
+    pong.state.ball.x += pong.state.ball.dx;
+    pong.state.ball.y += pong.state.ball.dy;
 }
 
 function updatePaddleLocation(paddle) {
@@ -113,7 +114,7 @@ function updatePaddleLocation(paddle) {
 
 function draw() {
     pong.ctx.clearRect(0, 0, pong.canvas.width, pong.canvas.height);
-    drawBall(pong.ctx, pong.state.x, pong.state.y, pong.constants.ballRadius, pong.constants.color);
+    drawBall(pong.ctx, pong.state.ball.x, pong.state.ball.y, pong.constants.ballRadius, pong.constants.color);
     drawPaddle(pong.ctx, pong.state.paddle1.x, pong.canvas.height - pong.constants.paddleHeight - pong.constants.paddlePadding, pong.constants.paddleHeight, pong.constants.paddleWidth, pong.constants.color);
     drawPaddle(pong.ctx, pong.state.paddle2.x, pong.constants.paddlePadding, pong.constants.paddleHeight, pong.constants.paddleWidth, pong.constants.color);
     checkCollisions();
